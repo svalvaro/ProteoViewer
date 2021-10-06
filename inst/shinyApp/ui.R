82226bb4f4fbdba3e37739a2a6d29d4e942e4255
svalvaro <sanchezvillalba.alvaro@gmail.com>
2021-10-06 12:38:47 +0200

    cleaner code

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -1,153 +1,131 @@
 dashboardPage(
+    dashboardHeader(
+        title =  titlePanel(
+            title = 'ProteoViewer',
 
-    # dashboardHeader(title = title,
-    #     # title = titlePanel(
-    #     #     #title = title,
-    #     #
-    #     #
-    #     #     windowTitle = tags$head(
-    #     #         tags$link(
-    #     #             rel = "icon", type = "image/png", href = "logo.png"
-    #     #             ),
-    #     #         tags$title("ProteoViewer")
-    #     #         )
-    #     #     ),
-    #     #logo_path = tag$img(src = system.file('shinyApp/www/logo.png', package = 'ProteoViewer')),
-    #
-    #
-    #
-    #     titleWidth =  300),
-
-    dashboardHeader(title =  titlePanel(
-                title = 'ProteoViewer',
-
-                windowTitle = tags$head(
-                    tags$link(
-                        rel = "icon", type = "image/png", href = "logo_small.png"
-                        ),
-                    tags$title("ProteoViewer")
-                    )
-                ),
-                tags$li(a(href = 'https://fgu.cas.cz/departments/proteomicka-servisni-laborator',
-                              icon("power-off"),
-                              title = "Close ProteoViewer"),
-                            class = "dropdown"),
-                tags$li(a(href = 'https://fgu.cas.cz/departments/proteomicka-servisni-laborator',
-                          img(src = 'logo.png',
-                              title = "ProteoLab", height = "30px"),
-                              style = "padding-top:10px; padding-bottom:10px;"),
-                            class = "dropdown")
+            windowTitle = tags$head(
+                tags$link(
+                    rel = "icon", type = "image/png", href = "logo_small.png"
                     ),
+                tags$title("ProteoViewer")
+                )
+            ),
+            tags$li(a(href = 'https://fgu.cas.cz/departments/proteomicka-servisni-laborator',
+                          icon("power-off"),
+                          title = "Close ProteoViewer"),
+                        class = "dropdown"),
+            tags$li(a(href = 'https://fgu.cas.cz/departments/proteomicka-servisni-laborator',
+                      img(src = 'logo.png',
+                          title = "ProteoLab", height = "30px"),
+                          style = "padding-top:10px; padding-bottom:10px;"),
+                        class = "dropdown")
+                ),
 
-    dashboardSidebar(width = 300,
+    dashboardSidebar(
+        width = 300,
 
         sidebarMenu(width = 300,
             id = 'tabs_selected',
 
             ProteoViewer:::convertMenuItem(
-                menuItem(text = 'Protein Visualization',
-                         tabName = 'proteinViz',
-                         selected = TRUE,
-                         startExpanded = TRUE,
+                menuItem(
+                    text = 'Protein Visualization',
 
-                         fileInput(inputId = "proteomics_table",
-                                   label =  h4("Upload the evidence.txt file"),
-                                   multiple = FALSE,
-                                   accept = 'text'
-                         ),
+                    tabName = 'proteinViz',
 
+                    selected = TRUE,
 
-                         # Select the protein of interest
-                         uiOutput('proteinsSelect'),
+                    startExpanded = TRUE,
 
+                    fileInput(inputId = "proteomics_table",
+                       label =  h4("Upload the evidence.txt file"),
+                       multiple = FALSE,
+                       accept = 'text'
+                    ),
 
+                    # Select the protein of interest
+                    uiOutput('proteinsSelect'),
 
+                    # Compare between conditions based on experiment design
+                    uiOutput('comparisonCheck'),
 
-                         # Compare between conditions based on experiment design
-                         uiOutput('comparisonCheck'),
-                         uiOutput('comparisonSelector'),
+                    uiOutput('comparisonSelector'),
 
-                         br(),
+                    br(),
 
-                         # checkboxInput('combineExperiments',
-                         #               h4('Combine all the experiments'),
-                         #               value = FALSE#,
-                         #               #icon("paper-plane"),
-                         #               #style="color: #fff; background-color: #337ab7; border-color: #2e6da4"
-                         # ),
+                    uiOutput('combineExperimentsOutput'),
 
+                    uiOutput('experimentSelect'),
 
+                    selectInput(
+                        inputId = 'proteaseSelected',
 
+                        label =  h4('Protease'),
 
-                         uiOutput('combineExperimentsOutput'),
+                        choices = c(
+                            'No protease' = 'none',
+                            'Trypsin' = 'Tryps',
+                            'LysC' = 'LysC',
+                            'LysN' = 'LysN',
+                            'Arg-C proteinase' = 'Arg-Cproteinase',
+                            'Asp-N endopeptidase' = 'Asp-Nendopeptidase',
+                            'Asp-N endopeptidase + N-terminal Glu' = 'Asp-Nendopeptidase+N-terminalGlu',
+                            'BNPS-Skatole' = 'BNPS-Skatole',
+                            'Caspase1' = 'Caspase1',
+                            'Caspase2' = 'Caspase2',
+                            'Caspase3' = 'Caspase3',
+                            'Caspase4' = 'Caspase4',
+                            'Caspase5' = 'Caspase5',
+                            'Caspase6' = 'Caspase6',
+                            'Caspase7' = 'Caspase7',
+                            'Caspase8' = 'Caspase8',
+                            'Caspase9' = 'Caspase9',
+                            'Caspase10' = 'Caspase10',
+                            'Chymotrypsin-high specificity' = 'Chymotrypsin-highspecificity',
+                            'Chymotrypsin-low specificity' = 'Chymotrypsin-lowspecificity',
+                            'Clostripain (Clostridiopeptidase B)' = 'Clostripain(ClostridiopeptidaseB)',
+                            'CNBr' = 'CNBr',
+                            'Enterokinase' = 'Enterokinase',
+                            'Factor Xa' = 'FactorXa',
+                            'Formic acid' = 'Formicacid',
+                            'Glutamyl endopeptidase' = 'Glutamylendopeptidase',
+                            'GranzymeB' = 'GranzymeB',
+                            'Hydroxylamine' = 'Hydroxylamine',
+                            'Iodosobenzoic acid' = 'Iodosobenzoicacid',
+                            'NTCB (2-nitro-5-thiocyanobenzoic acid)' = 'NTCB(2-nitro-5-thiocyanobenzoicacid)',
+                            'Pepsin (pH1.3)' = 'Pepsin(pH1.3)',
+                            'Pepsin (pH>2)' = 'Pepsin(pH>2)',
+                            'Proline-endopeptidase' = 'Proline-endopeptidase',
+                            'Proteinase K' = 'ProteinaseK',
+                            'Staphylococcal peptidase I' = 'StaphylococcalpeptidaseI',
+                            'Tobacco etch virus protease' = 'Tobaccoetchvirusprotease',
+                            'Thermolysin' = 'Thermolysin',
+                            'Thrombin' = 'Thrombin'),
 
-                         # checkboxInput('combineExperiments',
-                         #                h4('Combine all the experiments'),
-                         #                value = FALSE#,
-                         #                                 #icon("paper-plane"),
-                         #                                 #style="color: #fff; background-color: #337ab7; border-color: #2e6da4"
-                         #                   ),
+                        selected = 'none'),
 
-                           uiOutput('experimentSelect'),
+                    sliderInput(
+                        inputId = 'zoomFigure',
+                        label = h4('Select the size of the image'),
+                        min = 100, max = 5000, value = 1000)
 
-                           selectInput(inputId = 'proteaseSelected',
-                                       label =  h4('Protease'),
-                                       choices = c('No protease' = 'none',
-                                                   'Trypsin' = 'Tryps',
-                                                   'LysC' = 'LysC',
-                                                   'LysN' = 'LysN',
-                                                   'Arg-C proteinase' = 'Arg-Cproteinase',
-                                                   'Asp-N endopeptidase' = 'Asp-Nendopeptidase',
-                                                   'Asp-N endopeptidase + N-terminal Glu' = 'Asp-Nendopeptidase+N-terminalGlu',
-                                                   'BNPS-Skatole' = 'BNPS-Skatole',
-                                                   'Caspase1' = 'Caspase1',
-                                                   'Caspase2' = 'Caspase2',
-                                                   'Caspase3' = 'Caspase3',
-                                                   'Caspase4' = 'Caspase4',
-                                                   'Caspase5' = 'Caspase5',
-                                                   'Caspase6' = 'Caspase6',
-                                                   'Caspase7' = 'Caspase7',
-                                                   'Caspase8' = 'Caspase8',
-                                                   'Caspase9' = 'Caspase9',
-                                                   'Caspase10' = 'Caspase10',
-                                                   'Chymotrypsin-high specificity' = 'Chymotrypsin-highspecificity',
-                                                   'Chymotrypsin-low specificity' = 'Chymotrypsin-lowspecificity',
-                                                   'Clostripain (Clostridiopeptidase B)' = 'Clostripain(ClostridiopeptidaseB)',
-                                                   'CNBr' = 'CNBr',
-                                                   'Enterokinase' = 'Enterokinase',
-                                                   'Factor Xa' = 'FactorXa',
-                                                   'Formic acid' = 'Formicacid',
-                                                   'Glutamyl endopeptidase' = 'Glutamylendopeptidase',
-                                                   'GranzymeB' = 'GranzymeB',
-                                                   'Hydroxylamine' = 'Hydroxylamine',
-                                                   'Iodosobenzoic acid' = 'Iodosobenzoicacid',
-                                                   'NTCB (2-nitro-5-thiocyanobenzoic acid)' = 'NTCB(2-nitro-5-thiocyanobenzoicacid)',
-                                                   'Pepsin (pH1.3)' = 'Pepsin(pH1.3)',
-                                                   'Pepsin (pH>2)' = 'Pepsin(pH>2)',
-                                                   'Proline-endopeptidase' = 'Proline-endopeptidase',
-                                                   'Proteinase K' = 'ProteinaseK',
-                                                   'Staphylococcal peptidase I' = 'StaphylococcalpeptidaseI',
-                                                   'Tobacco etch virus protease' = 'Tobaccoetchvirusprotease',
-                                                   'Thermolysin' = 'Thermolysin',
-                                                   'Thrombin' = 'Thrombin'),
-                                       selected = 'none'),
+                    ),
+                tabName = 'proteinviz'
 
-                           sliderInput(inputId = 'zoomFigure',
-                                       label = h4('Select the size of the image'),
-                                       min = 100, max = 5000, value = 1000)
-
-                ), tabName = 'proteinviz'
-            ),
+                ),
 
             ProteoViewer:::convertMenuItem(
 
-                menuItem('Experiment Design', tabName = 'experimentDesignTab',
+                menuItem(
+                    'Experiment Design',
+                    tabName = 'experimentDesignTab',
 
-                         fileInput(inputId = "expDesignUpload",
-                                   label =  h4("Upload the experiment design file"),
-                                   multiple = FALSE,
-                                   accept = 'text')
-                         ), tabName = 'expDesigntab'
+                    fileInput(inputId = "expDesignUpload",
+                        label =  h4("Upload the experiment design file"),
+                        multiple = FALSE,
+                        accept = 'text')
+                    ),
+                tabName = 'expDesigntab'
                 )
             ),
 
@@ -158,8 +136,7 @@ dashboardPage(
                                  color = "default",
                                  size = "md",
                                  block = FALSE,
-                                 no_outline = TRUE
-        )
+                                 no_outline = TRUE)
         ),
 
     dashboardBody(
@@ -183,10 +160,6 @@ dashboardPage(
                         uiOutput(outputId = 'proteinImageNoComparison'),
 
                         plotOutput('legend'),
-
-
-
-
 
                         # If two comparisons are selected:
                         box(title = h3(textOutput('titleProteinComparisonOne')),
