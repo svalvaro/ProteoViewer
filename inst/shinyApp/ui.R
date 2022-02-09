b9a05580380e7432ba6212f104cb6d771e88f1c3
svalvaro <sanchezvillalba.alvaro@gmail.com>
2022-02-09 14:06:58 +0100

    added demo data info

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -19,10 +19,7 @@ dashboardPage(
                 tags$script(src = "https://unpkg.com/panzoom@9.4.0/dist/panzoom.min.js"),
 
                 # Custom
-                tags$link(rel = "stylesheet", type = "text/css", href = "css/styles.css"),
-
-
-
+                tags$link(rel = "stylesheet", type = "text/css", href = "css/styles.css")
                 )
             ),
         tags$li(a(id = 'power-off',
@@ -88,22 +85,7 @@ dashboardPage(
 
                     uiOutput('proteaseSelector'),
 
-                    uiOutput('imageSizeSelector'),
-
-
-                    br(),
-                    br(),
-
-                    shinyWidgets::actionBttn(
-                        inputId = 'Demo',
-                        label = 'Load demo data',
-                        icon = NULL,
-                        style = "unite",
-                        color = "default",
-                        size = "md",
-                        block = FALSE,
-                        no_outline = TRUE)
-
+                    uiOutput('imageSizeSelector')
 
                     ),
 
@@ -122,7 +104,28 @@ dashboardPage(
                         accept = 'text')
                     ),
                 tabName = 'expDesigntab'
-                )
+                ),
+
+
+            ProteoViewer:::convertMenuItem(
+
+                menuItem(
+                    h3('Demo Data'),
+                    tabName = 'demoTab',
+
+                    shinyWidgets::actionBttn(
+                        inputId = 'Demo',
+                        label = 'Load demo data',
+                        icon = NULL,
+                        style = "unite",
+                        color = "default",
+                        size = "md",
+                        block = FALSE,
+                        no_outline = TRUE)
+
+                ),
+                tabName = 'demoDataTab'
+            )
             )
 
 
@@ -187,6 +190,67 @@ dashboardPage(
                             'experimentDesignOutput')
                         )
                     ),
+                hr(style = "border-color: #cbcbcb;"),
+
+                fluidRow(
+                    column(9,
+
+                           p("App created by ",
+                             tags$a(href = "https://www.linkedin.com/in/svalvaro/",
+                                    'Alvaro Sanchez-Villalba',
+                                    target = '_blank'),
+                             HTML("&bull;"),
+                             style = "font-size: 85%"),
+
+                           p("Have a question? Spot an error? Send an email ",
+                             tags$a(href = "mailto:alvaro.sanchez@fgu.cas.cz",
+                                    tags$i(class = 'fa fa-envelope',
+                                           style = 'color:#990000'),
+                                    target = '_blank'),
+                             style = "font-size: 85%"),
+                           p(tags$em("Last updated: February 2022"),
+                             style = 'font-size:75%')
+                           )
+                    )
+                ),
+
+
+            tabItem(
+
+                tabName = 'demoTab',
+
+                fluidRow(
+
+                    column(
+                        width = 12,
+                        h3("Proteomics Data"),
+
+
+                        h4(
+                            "ProteoViewer requires the evidence.txt file created by MaxQuant.
+                      You can download an example of this file here: "
+                        ),
+
+                        tags$a(href='data/evidence.txt',
+                               h4('evidence.txt'),
+                               download=NA,
+                               target='_blank'),
+
+                        h3("Experiment design"),
+                        h4("If you want to compare different conditions, you can do so
+                      by editing the table in Experiment Design. Alternatively,
+                      you can also upload an experiment design table, in .txt format.
+                      You can download an example of the experiment_design.txt here:"
+                        ),
+
+                        tags$a(href='data/experiment_design_example.txt',
+                               h4('experiment_design.txt'),
+                               download=NA,
+                               target='_blank'),
+                        h4("By clicking on Load demo data, these two files will be uploaded automatically")
+                    )
+                    ),
+
                 hr(style = "border-color: #cbcbcb;"),
 
                 fluidRow(
